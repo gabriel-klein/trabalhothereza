2e868312cb4f59025512064a100ba9b20c7bbe49
IsraelSouza9 <51099010+IsraelSouza9@users.noreply.github.com>
2019-05-27 19:27:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Smart Fitness</title>
+    </head>
+    <nav>
+        <ul>
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#quemsomos">Quem Somos</a></li>
+            <li><a href="#unidades">Unidades</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="#contato">Contato</a></li>
+        </ul>
+    </nav>
+    <body>
+        
+    </body>
+</html>
